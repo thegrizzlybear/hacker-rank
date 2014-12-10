62f6634169cbcdecdbf58c97e2e59f5e80a8582b
Uddhav Arote <uddhava@cse.iitb.ac.in>
2014-12-10 12:46:07 +0530

    modified utopian to run faster

--- utopian-tree.py
+++ utopian-tree.py
@@ -2,7 +2,7 @@
 	Program to find the length of the utopian tree
 
 '''
-
+'''
 if __name__ == '__main__':
 	
 	length= 1
@@ -23,6 +23,31 @@ if __name__ == '__main__':
 	for i in range(test_cases):
 		cycles.append(input())
 
+
+	for i in cycles:
+		print tree_length[i]
+'''
+
+if __name__ == '__main__':
+	test_cases = input()
+	cycles = []
+
+	for i in range(test_cases):
+		cycles.append(input())
+
+	max_cycle = max(cycles)+1
+
+	length = 1
+	tree_length = []
+
+	for i in range(max_cycle):
+		if i == 0:
+			pass
+		elif i%2 != 0:
+			length = length *2
+		else:
+			length += 1
+		tree_length.append(length)
 
 	for i in cycles:
 		print tree_length[i]
